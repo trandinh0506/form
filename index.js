b813687f3b401fa207b712b437be5cab4d15908e
Dinh Tran <tvandinh93@gmail.com>
2022-11-05 13:12:10 +0700

    done

---
+++ index.js
@@ -0,0 +1,32 @@
+const express = require("express");
+const fs = require("fs");
+const path = require("path");
+
+const PORT = process.env.PORT || 3000;
+
+const app = express();
+
+app.use(express.json());
+
+var datas = [];
+fs.readFileSync(path.join(__dirname, "data.txt"), "utf8", (err, data) => {
+  datas = JSON.parse(data);
+});
+app.get("/", (req, res) => {
+  res.send("success");
+});
+
+app.post("register", (req, res) => {
+  let player = req.body.player;
+  let group = req.body.group;
+  datas.push({ player, group });
+  fs.writeFileSync(path.join(__dirname, "data.txt"), JSON.stringify(datas));
+});
+
+app.get("/getdatablabla", (req, res) => {
+  res.sendFile(path.join(__dirname, "data.txt"));
+});
+
+app.listen(PORT, () => {
+  console.log(`http://localhost:${PORT}`);
+});
