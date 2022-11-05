826f7264bff1f7b00971d5d99bea4c17f53f26d4
Dinh Tran <tvandinh93@gmail.com>
2022-11-05 15:53:38 +0700

    ok

--- index.js
+++ index.js
@@ -12,11 +12,8 @@ var datas = [];
 fs.readFileSync(path.join(__dirname, "data.txt"), "utf8", (err, data) => {
   datas = JSON.parse(data);
 });
-app.get("/", (req, res) => {
-  res.send("success");
-});
 
-app.post("register", (req, res) => {
+app.post("/register", (req, res) => {
   let player = req.body.player;
   let group = req.body.group;
   datas.push({ player, group });
@@ -24,10 +21,9 @@ app.post("register", (req, res) => {
   res.send("success");
 });
 
-app.get("/getdatablabla", (req, res) => {
-  res.sendFile(path.join(__dirname, "data.txt"));
+app.get("/", (req, res) => {
+  res.send("");
 });
-
 app.listen(PORT, () => {
   console.log(`http://localhost:${PORT}`);
 });
